cc3d42fe99755de97377a38f31ea0c687d2a0895
den7466 <92376732+den7466@users.noreply.github.com>
2021-12-29 19:38:19 +0300

    Add js for pop-up and edit css

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Pop-Up
+function openPopUp() {
+    document.getElementById("pop-up").style.display = "block";
+}
+
+function closePopUp() {
+    document.getElementById("pop-up").style.display = "none";
+}
