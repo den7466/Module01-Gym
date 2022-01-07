9f5603be5fe012f03f25b70dc672f71a7738fcf4
den7466 <92376732+den7466@users.noreply.github.com>
2022-01-07 11:43:51 +0300

    Edit css for mobile

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,12 @@ function openPopUp() {
 function closePopUp() {
     document.getElementById("pop-up").style.display = "none";
 }
+
+// Mobile-Menu
+function openMobMenu() {
+    document.getElementById("mob-menu").style.display = "flex";
+}
+
+function closeMobMenu() {
+    document.getElementById("mob-menu").style.display = "none";
+}
