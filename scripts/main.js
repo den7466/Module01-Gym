7ea5ccb3390c68c2afe9c99dfa26aaefdb76533f
den7466 <92376732+den7466@users.noreply.github.com>
2022-01-10 19:41:41 +0300

    Edit css and html structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+// Smooth Scroll
+      
+const links = document.querySelectorAll(".banner-arrow");
+
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
 // Pop-Up
 function openPopUp() {
     document.getElementById("pop-up").style.display = "block";
@@ -9,7 +26,7 @@ function closePopUp() {
 
 // Mobile-Menu
 function openMobMenu() {
-    document.getElementById("mob-menu").style.display = "flex";
+    document.getElementById("mob-menu").style.display = "block";
 }
 
 function closeMobMenu() {
